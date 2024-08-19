87e69e576dcbd17a7c4c29b1f35355c7f7d629dd
Geran. <113444430+gurainzu@users.noreply.github.com>
2024-08-19 09:13:41 +0700

    Add files via upload

--- script.sh
+++ script.sh
@@ -11,7 +11,7 @@ echo -e "Normal \e[1mCloning and Updating Repository"
 cd /home/antix
 git clone https://github.com/gurainzu/skripkonyol
 mv /etc/apt/sources.list /etc/apt/sources.list-back
-mv /home/antix/skripkonyol/sources.list /etc/apt/
+mv /home/antix/skripkonyol/repository/sources.list /etc/apt/
 apt update -y
 
 #Installing BIND9
@@ -23,8 +23,8 @@ echo -e "Normal \e[1mInstalling Complete, configuring BIND9"
 echo -e "Normal \e[1mCopying Configuration"
 mv /etc/bind/named.conf.local /etc/bind/named.comf.local-back
 mv /etc/bind/named.conf.options /etc/bind/named.conf.options-back
-mv /home/antix/skripkonyol/db.172 /etc/bind/
-mv /home/antix/skripkonyol/db.absen /etc/bind/
-mv /home/antix/skripkonyol/db.usk /etc/bind/
+mv /home/antix/skripkonyol/bind9conf/db.172 /etc/bind/
+mv /home/antix/skripkonyol/bind9conf/db.absen /etc/bind/
+mv /home/antix/skripkonyol/bind9conf/db.usk /etc/bind/
 echo -e "Normal \e[1mRestarting Services, generating status when complete."
 systemctl status bind9
