4205ba934116db2eac5328fe86dc3407cde73c7e
Geran. <113444430+gurainzu@users.noreply.github.com>
2024-08-19 09:36:01 +0700

    Add files via upload

--- script.sh
+++ script.sh
@@ -7,24 +7,33 @@ if [ "$EUID" -ne 0 ]
 fi
 
 #Cloning Repository
-echo -e "Normal \e[1mCloning and Updating Repository"
+echo -e "\e[92mStarting \e[1mCloning and Updating Repository"
 cd /home/antix
 git clone https://github.com/gurainzu/skripkonyol
 mv /etc/apt/sources.list /etc/apt/sources.list-back
 mv /home/antix/skripkonyol/repository/sources.list /etc/apt/
 apt update -y
+echo -e "\e[92mComplete \e[1mCloning and Updating Repository"
 
 #Installing BIND9
-echo -e "Normal \e[1mInstalling BIND9"
+echo -e "\e[92mStarting \e[1mInstalling BIND9"
 apt install bind9 bind9utils dnsutils -y
-echo -e "Normal \e[1mInstalling Complete, configuring BIND9"
+echo -e "\e[92mComplete \e[1mInstalling Complete, configuring BIND9"
 
 #Configuring BIND9
-echo -e "Normal \e[1mCopying Configuration"
+echo -e "\e[92mStarting \e[1mCopying Configuration"
 mv /etc/bind/named.conf.local /etc/bind/named.comf.local-back
 mv /etc/bind/named.conf.options /etc/bind/named.conf.options-back
-mv /home/antix/skripkonyol/bind9conf/db.172 /etc/bind/
+mv /home/antix/skripkonyol/bind9conf/db.192 /etc/bind/
 mv /home/antix/skripkonyol/bind9conf/db.absen /etc/bind/
 mv /home/antix/skripkonyol/bind9conf/db.usk /etc/bind/
-echo -e "Normal \e[1mRestarting Services, generating status when complete."
+mv /home/antix/skripkonyol/bind9conf/named.conf.local /etc/bind/
+mv /home/antix/skripkonyol/bind9conf/named.conf.options /etc/bind/
+echo -e "\e[92mComplete \e[1mRestarting Services, generating status when complete."
+systemctl restart bind9
 systemctl status bind9
+
+#Installing Apache2
+echo -e "\e[92mStarting \e[1mInstalling Apache2"
+apt install apache2 -y
+echo -e "\e[92mComplete \e[1mInstalling BIND9"
