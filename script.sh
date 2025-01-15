b58a358963699018f91a0ee29f4b92e2f7b77f5f
Geran. <113444430+gurainzu@users.noreply.github.com>
2025-01-15 13:53:21 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -16,27 +16,32 @@ fi
 #echo -e "\e[92mComplete \e[1mCloning and Updating Repository"
 
 #Installing BIND9
+sleep 2
 echo -e "\e[92mStarting \e[1mInstalling BIND9"
 apt install bind9 bind9utils dnsutils -y
 echo -e "\e[92mComplete \e[1mInstalling Complete, configuring BIND9"
+sleep 2
 
 #Configuring BIND9
 echo -e "\e[92mStarting \e[1mCopying Configuration"
 mv /etc/bind/named.conf.local /etc/bind/named.comf.local-back
 mv /etc/bind/named.conf.options /etc/bind/named.conf.options-back
-mv /home/antix/skripkonyol/bind9conf/db.192 /etc/bind/
-mv /home/antix/skripkonyol/bind9conf/db.absen /etc/bind/
-mv /home/antix/skripkonyol/bind9conf/db.usk /etc/bind/
-mv /home/antix/skripkonyol/bind9conf/named.conf.local /etc/bind/
-mv /home/antix/skripkonyol/bind9conf/named.conf.options /etc/bind/
+mv /home/antix/skripkonyol/bind9-conf/db.192 /etc/bind/
+mv /home/antix/skripkonyol/bind9-conf/db.172 /etc/bind/
+mv /home/antix/skripkonyol/bind9-conf/db.absen /etc/bind/
+mv /home/antix/skripkonyol/bind9-conf/db.usk /etc/bind/
+mv /home/antix/skripkonyol/bind9-conf/named.conf.local /etc/bind/
+mv /home/antix/skripkonyol/bind9-conf/named.conf.options /etc/bind/
+sleep 2
 echo -e "\e[92mComplete \e[1mRestarting Services, generating status when complete."
 systemctl restart bind9
-systemctl status bind9
 
 #Installing Apache2
+sleep 2
 echo -e "\e[92mStarting \e[1mInstalling Apache2"
 apt install apache2 -y
 echo -e "\e[92mComplete \e[1mInstalling Apache2"
+sleep 2
 
 #Installing PHP/PMA
 echo -e "\e[92mStarting \e[1mInstalling PHP/PMA"
@@ -49,10 +54,13 @@ apt install mariadb-server -y
 echo -e "\e[92mComplete \e[1mInstalling MariaDB Server"
 
 #Configuring MariaDB Server
+sleep 2
 echo -e "\e[92mStarting \e[1mConfiguring MariaDB Server"
-mysql -e "CREATE DATABASE IF NOT EXISTS wp-absen"
-mysql -e "CREATE DATABASE IF NOT EXISTS wp-usk"
+mysqlidentity = "-u root -p antix"
+mysql $mysqlidentity -e "CREATE DATABASE IF NOT EXISTS wp-absen"
+mysql $mysqlidentity -e "CREATE DATABASE IF NOT EXISTS wp-usk"
 echo -e "\e[92mComplete \e[1mConfiguring MariaDB Server"
+sleep 2
 
 #Downloading Wordpress in Apache
 echo -e "\e[92mStarting \e[1mDownloading Wordpress in Apache"
@@ -64,16 +72,17 @@ unzip latest.zip && mv wordpress wp-absen
 echo -e "\e[92mComplete \e[1mDownloading Wordpress in Apache"
 
 #Configuring Virtual Host in Apache
-mv /home/antix/skripkonyol/apacheconf/wp-usk.conf /etc/apache2/sites-available/
-mv /home/antix/skripkonyol/apacheconf/wp-absen.conf /etc/apache2/sites-available/
-mv /home/antix/skripkonyol/apacheconf/pma-usk.conf /etc/apache2/sites-available/
-mv /home/antix/skripkonyol/apacheconf/pma-absen.conf /etc/apache2/sites-available/
-mv /home/antix/skripkonyol/apacheconf/mail-absen.conf /etc/apache2/sites-available/
-mv /home/antix/skripkonyol/apacheconf/cacti-usk.conf /etc/apache2/sites-available/
+mv /home/antix/skripkonyol/apache-conf/wp-usk.conf /etc/apache2/sites-available/
+mv /home/antix/skripkonyol/apache-conf/wp-absen.conf /etc/apache2/sites-available/
+mv /home/antix/skripkonyol/apache-conf/pma-usk.conf /etc/apache2/sites-available/
+mv /home/antix/skripkonyol/apache-conf/pma-absen.conf /etc/apache2/sites-available/
+mv /home/antix/skripkonyol/apache-conf/mail-absen.conf /etc/apache2/sites-available/
+mv /home/antix/skripkonyol/apache-conf/cacti-usk.conf /etc/apache2/sites-available/
 a2ensite wp-usk.conf
 a2ensite wp-absen.conf
 a2ensite pma-usk.conf
 a2ensite pma-absen.conf
 a2ensite mail-absen.conf
 a2ensite cacti-usk.conf
-systemctl restart apache2 && systemctl status apache2
+systemctl restart apache2
+echo -e "\e[92mComplete \e[1mConfiguring VirtualHost in Apache"
