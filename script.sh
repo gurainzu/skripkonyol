a6dc941132c0ba777d0a3b9ddf3cd3ee53f80cea
Geran. <113444430+gurainzu@users.noreply.github.com>
2025-01-16 23:01:44 +0700

    Add files via upload

--- script.sh
+++ script.sh
@@ -22,8 +22,8 @@ read -p "Enter the name of your db file, example=db.usk: " dbfile1
 cp /home/antix/skripkonyol/bind9-conf/db.local /etc/bind/$dbfile1
 read -p "Enter the name of your second db file, example=db.absen: " dbfile2
 cp /home/antix/skripkonyol/bind9-conf/db.local /etc/bind/$dbfile2
-read -p "Enter the name of your db reverse file, example=db.172: " dbfilereverse
-cp /home/antix/skripkonyol/bind9-conf/db.reverse /etc/bind/$dbfilereverse
+read -p "Enter the name of your db reverse file, example=db.172: " filedbreverse
+cp /home/antix/skripkonyol/bind9-conf/db.reverse /etc/bind/$filedbreverse
 cp /home/antix/skripkonyol/bind9-conf/named.conf.local /etc/bind/
 cp /home/antix/skripkonyol/bind9-conf/named.conf.options /etc/bind/
 sleep 2
@@ -44,12 +44,12 @@ read -p "Enter your IP for your second db file, example=172.16.31.10: " ipdomain
 sed -i "s/IP/$ipdomain2/g" /etc/bind/$dbfile2
 sleep 2
 read -p "Enter your reverse IP that you use in first db file, example=10.31.16: " ipreverse1
-sed -i "s/domain/$domain1/g" /etc/bind/$dbfilereverse
-sed -i "s/reverseIP/$ipreverse1/g" /etc/bind/$dbfilereverse
+sed -i "s/domain/$domain1/g" /etc/bind/$filedbreverse
+sed -i "s/reverseIP/$ipreverse1/g" /etc/bind/$filedbreverse
 sleep 2
 read -p "Enter your reverse IP that you use in second db file, example=10.31.16: " ipreverse2
-sed -i "s/DOMAIN/$domain2/g" /etc/bind/$dbfilereverse
-sed -i "s/kedua/$ipreverse2/g" /etc/bind/$dbfilereverse
+sed -i "s/DOMAIN/$domain2/g" /etc/bind/$filedbreverse
+sed -i "s/kedua/$ipreverse2/g" /etc/bind/$filedbreverse
 sleep 2
 read -p "Enter the first block of your IP, example=172: " firstblock
 sed -i "s/domain/$domain1/g" /etc/bind/named.conf.local
@@ -57,43 +57,65 @@ sed -i "s/DOMAIN/$domain2/g" /etc/bind/named.conf.local
 sed -i "s/firstblock/$firstblock/g" /etc/bind/named.conf.local
 sed -i "s/dbfile/$dbfile1/g" /etc/bind/named.conf.local
 sed -i "s/DBFILE/$dbfile2/g" /etc/bind/named.conf.local
-sed -i "s/dbfilereverse/$dbfilereverse/g" /etc/bind/named.conf.local
-echo "Success replacing domain and IP for your BIND Configuration, Restarting...."
+sed -i "s/filedbreverse/$filedbreverse/g" /etc/bind/named.conf.local
 systemctl restart bind9
+echo "Success replacing domain and IP for your BIND Configuration"
+
+sleep 5
+clear
 
 #Installing Apache2
-sleep 2
-echo -e "\e[92mStarting \e[1mInstalling Apache2"
+echo "Starting Installing Apache2"
 apt install apache2 -y
-echo -e "\e[92mComplete \e[1mInstalling Apache2"
-sleep 2
+echo "Complete Installing Apache2"
+
+sleep 5
+clear
 
 #Installing PHP/PMA
-echo -e "\e[92mStarting \e[1mInstalling PHP/PMA"
+echo "Starting Installing PHP/PMA"
 apt install php php-mysql php-json phpmyadmin -y
-echo -e "\e[92mComplete \e[1mInstalling PHP/PMA"
+echo "Complete Installing PHP/PMA"
+
+sleep 5
+clear
 
 #Installing MariaDB Server
-echo -e "\e[92mStarting \e[1mInstalling MariaDB Server"
+echo "Starting Installing MariaDB Server"
 apt install mariadb-server -y
-echo -e "\e[92mComplete \e[1mInstalling MariaDB Server"
+echo "Complete Installing MariaDB Server"
+
+sleep 5
+clear
 
 #Configuring MariaDB Server
+echo "Starting Configuring MariaDB Server"
+read -p "Enter the first database name, example=wpusk: " database
+echo "Please enter the MYSQL Password"
+mysqladmin -u root -p create $database;
 sleep 2
-echo -e "\e[92mStarting \e[1mConfiguring MariaDB Server"
-mysqladmin -u root -p create wpabsen;
-mysqladmin -u root -p create wpusk;
-echo -e "\e[92mComplete \e[1mConfiguring MariaDB Server"
+read -p "Enter the second database name, example=wpabsen: " DATABASE
+mysqladmin -u root -p create $DATABASE;
+echo "Please enter the MYSQL Password"
 sleep 2
+echo "Complete Configuring MariaDB Server"
+
+sleep 5
+clear
 
 #Downloading Wordpress in Apache
-echo -e "\e[92mStarting \e[1mDownloading Wordpress in Apache"
+echo "Starting Downloading Wordpress in Apache"
+sleep 2
 apt install unzip wget -y
 cd /var/www/
 wget https://wordpress.org/latest.zip
 unzip latest.zip && mv wordpress wp-usk
 unzip latest.zip && mv wordpress wp-absen
-echo -e "\e[92mComplete \e[1mDownloading Wordpress in Apache"
+sleep 2
+echo "Complete Downloading Wordpress in Apache"
+
+sleep 5
+clear
 
 #Configuring Virtual Host in Apache
 mv /home/antix/skripkonyol/apache-conf/wp-usk.conf /etc/apache2/sites-available/
@@ -109,4 +131,5 @@ a2ensite pma-absen.conf
 a2ensite mail-absen.conf
 a2ensite cacti-usk.conf
 systemctl restart apache2
-echo -e "\e[92mComplete \e[1mConfiguring VirtualHost in Apache"
+sleep 2
+echo "Complete \e[1mConfiguring VirtualHost in Apache"
